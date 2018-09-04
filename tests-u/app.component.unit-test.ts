d49f0b152fd3683d5fcc3176989c674439d0cb02
Niel Hekkens <nielhekkens@gmail.com>
2018-09-04 21:36:27 +0200

    Clean up project

--- tests-u/app.component.unit-test.ts
+++ tests-u/app.component.unit-test.ts
@@ -7,18 +7,8 @@ import { AppComponent } from "src/app/app.component";
 @suite
 class AppComontentTest {
 
-	// private _testInstanceMock: Moq.IMock<AppComponent>;
-
-	// private get testInstanceMock(): Moq.IMock<AppComponent> {
-	// 	return this._testInstanceMock;
-	// }
-
-	// private get testInstance(): AppComponent {
-	// 	return this.testInstanceMock.object;
-	// }
-
 	@test public Test() {
 		const isTrue = true;
-		assert.isTrue(false);
+		assert.isTrue(isTrue);
 	}
 }
