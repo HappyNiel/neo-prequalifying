a322e09120a32c258e57606760157468b48c25b0
Niel Hekkens <nielhekkens@gmail.com>
2019-09-15 23:11:41 +0200

    results session 3

--- src/app/services/session-data.service.ts
+++ src/app/services/session-data.service.ts
@@ -1,7 +1,7 @@
 import * as dataSession1 from "../../json/24hseries/results-session1.json";
 import * as dataSession2 from "../../json/24hseries/results-session2.json";
-import * as dataSession3 from "../../json/session3-results.json";
-import * as dataSession4 from "../../json/session4-results.json";
+import * as dataSession3 from "../../json/24hseries/results-session3.json";
+import * as dataSession4 from "../../json/24hseries/results-session4.json";
 import { DriverInfo } from "../classes/driver-data.model";
 
 export class SessionData {
@@ -17,8 +17,8 @@ export class SessionData {
 	public sessions = [
 		{ id: 1, sessionId: 10, startTime: "08:00", sessionResults: dataSession1["driverData"], parsedSessionResults: this.parsedDataSession1},
 		{ id: 2, sessionId: 20, startTime: "12:00", sessionResults: dataSession2["driverData"], parsedSessionResults: this.parsedDataSession2},
-		// { id: 3, sessionId: 8901, startTime: "17:00", sessionResults: dataSession3["driverData"], parsedSessionResults: this.parsedDataSession3},
-		// { id: 4, sessionId: 2345, startTime: "20:00", sessionResults: dataSession4["driverData"], parsedSessionResults: this.parsedDataSession4}
+		{ id: 3, sessionId: 30, startTime: "17:00", sessionResults: dataSession3["driverData"], parsedSessionResults: this.parsedDataSession3},
+		// { id: 4, sessionId: 40, startTime: "20:00", sessionResults: dataSession4["driverData"], parsedSessionResults: this.parsedDataSession4}
 	];
 
 	public getSessionStartTime(id: number): string {
